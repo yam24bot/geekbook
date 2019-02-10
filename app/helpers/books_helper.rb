5b447fabd4aa8c067f2a9e29cc8daa44feb8a709
yam24bot <danil180401@gmail.com>
2019-02-10 18:56:21 +0200

    Add image uploader to book

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,10 @@
 module BooksHelper
+  def envelope_for(book, options = {size: 80})
+    size = options[:size]
+    if book.envelope?
+      image_tag book.envelope.url(:thumb), width: size
+    else
+      image_tag 'no_image.jpg', width: size
+    end
+  end
 end
