d686694b6b7ad19a91e6f5111cd0a511d36be3ef
yam24bot <danil180401@gmail.com>
2019-02-11 00:22:12 +0200

    Add admin

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'mini_magick' #
 gem 'pg', '>= 0.18', '< 2.0' #
 gem 'puma', '~> 3.11'
 gem 'rails', '~> 5.2.2'
+gem 'rails_admin' #
 gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   root 'books#index'
 
-  # resources :books
+  resources :books
   resources :users
   resources :tags, only: [:show]
-
-  resources :books do
-    member do
-      put 'update_draft_status'
-    end
-  end
 
 end
